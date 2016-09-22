47807772306ca0498ae546db930d26a8abee0844
CurationDockerEngine <felix@cloudburo.net>
2016-09-22 16:50:23 +0000

    New Posts

--- js/app.js
+++
@@ -1,1 +0,0 @@
-(function(){!function(t){return t(window).on("load",function(){return t(document).delegate('*[data-toggle="lightbox"]',"click",function(n){return n.preventDefault(),t(this).ekkoLightbox()})})}(jQuery)}).call(this);
